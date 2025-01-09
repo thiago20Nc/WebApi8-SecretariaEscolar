90f50016fcac04f628481a96c99d6d83f202b859
Thiago Novaes <novaesthiago27@gmail.com>
2025-01-09 16:16:28 -0300

    Banco de dados criado e conexão estaelecida

--- WebApi8-SecretariaEscolar/Data/AppDbContext.cs
+++ WebApi8-SecretariaEscolar/Data/AppDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApi8_SecretariaEscolar.Models;
 
 namespace WebApi8_SecretariaEscolar.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-            
-        }
+        {    
+        } 
+
+        public DbSet<ProfessorModel> Professor { get; set; }
+        public DbSet<TurmaModel> Turma { get; set; }
+        public DbSet<AlunoModel> Aluno { get; set; }
     }
 }
---
+++ WebApi8-SecretariaEscolar/Migrations/20250109190050_CriandoBancoDeDados.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi8_SecretariaEscolar.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoBancoDeDados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Diciplina = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turma",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfessorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turma", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Turma_Professor_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aluno",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TurmaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluno", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Aluno_Turma_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "Turma",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aluno_TurmaId",
+                table: "Aluno",
+                column: "TurmaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Turma_ProfessorId",
+                table: "Turma",
+                column: "ProfessorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aluno");
+
+            migrationBuilder.DropTable(
+                name: "Turma");
+
+            migrationBuilder.DropTable(
+                name: "Professor");
+        }
+    }
+}
